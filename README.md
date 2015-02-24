576be015f1d46814deb793bdf6a178676a7f3c44
Pedro Lopes <pedro.secinfo@gmail.com>
2015-02-23 22:41:32 -0300

    extension

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # area31-Chrome-Extension-agenda-de-atividades
 Extensao do chrome com um popup buscando a Agenda de Atividades do Area31 Hackerspace.
+
+Tive uma ideia simples, seguindo base em outra extensao aberta.
+
+Basta ir em chrome://extensions/ > Marcar "Devel Mode" > e carregar extensao nao packed.
+
+Se o projeto vingar, eh facil packear a extensao e sharear :)
+
+Vejo vcs no lado escuro da lua
---
+++ js/app/content.js
@@ -0,0 +1,22 @@
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+
+    if (request.action == 'PageInfo') {
+        var pageInfos = [];
+
+        $('a').each(function() {
+            var pageInfo = {};
+            
+            var href = $(this).attr('href');
+
+            if (href != null && href.indexOf("http") == 0)
+            {
+                //only add urls that start with http
+                pageInfo.url = href
+                pageInfos.push(pageInfo);
+            }
+        });
+
+        sendResponse(pageInfos);
+    }
+});
